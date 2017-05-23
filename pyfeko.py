a80dce0b0de4324ea05cee61ab489617260d71ca
U1and0 <e01.ando60@gmail.com>
2017-05-23 16:16:22 +0900

    [Add] rolling around test

--- pyfeko.py
+++ pyfeko.py
@@ -306,14 +306,4 @@ for c in cs:
 # 使い方: 区間2の移動平均線 : print(df.rolling_around(2))
 
 if __name__ == '__main__':
-    n = 3
-    df = pd.DataFrame(np.arange(n * 10).reshape(-1, n), columns=list('abc'))
-    window = 2
-
-    a = df.copy()
-    normal_rolling_mean = a.rolling(window).mean()
-    print('original\n', df)
-    print('normal rolling mean\n', normal_rolling_mean)
-    print('around rolling mean mirror \n', df.rolling_around(2, mirror=True))
-    print('around rolling mean NOT mirror\n',
-          df.rolling_around(2, mirror=False))  # mirror=False省略可
+    pass
